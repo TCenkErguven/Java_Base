ac32e8ed1763a45016f40e8fdb5b5600c21f23b8
Tunç Cenk Ergüven <erguven.cenk@gmail.com>
2024-11-29 11:32:40 +0300

    Table update

--- hazelcast-service/src/main/resources/schema.sql
+++ hazelcast-service/src/main/resources/schema.sql
@@ -1,11 +1,9 @@
-CREATE TABLE custom (
+CREATE TABLE DuplicateOperations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    message JSONB,
-    transaction_uuid VARCHAR(255) NOT NULL,
-    is_progress_completed BOOLEAN DEFAULT FALSE,
-    created BIGINT DEFAULT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP)::BIGINT,
-    updated BIGINT DEFAULT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP)::BIGINT,
-    deleted BIGINT,
+    request_message JSONB,
+    operation_uuid VARCHAR(255) NOT NULL,
+    created_date DATE DEFAULT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP)::BIGINT,
+    updated_date DATE,
     created_by VARCHAR(255),
     updated_by VARCHAR(255)
 );
